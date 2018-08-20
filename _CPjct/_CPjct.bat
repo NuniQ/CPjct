09d0cc54b0e3e6d7842d20def07dda0c9eaa96e8
Niktia <34923867+NuniQ@users.noreply.github.com>
2018-08-21 01:02:43 +0300

    1-st version of this repository
    Update 1.1

---
+++ _CPjct/_CPjct.bat
@@ -0,0 +1,30 @@
+set Current_CD_CPjct=%cd%
+cd C:\Users\User\Desktop
+rd /s /q  _project
+mkdir _project
+cd _project
+<nul set /p x=>index.html
+mkdir css
+mkdir js
+mkdir less
+cd less
+<nul set /p x=>style.less
+cd ..
+cd js
+<nul set /p x=>script.js
+cd ..
+set Current_CD__project=%cd%
+cd %Current_CD_CPjct%
+copy reset.css %Current_CD__project%/css
+copy index.html %Current_CD__project%
+copy server.bat %Current_CD__project%
+cd %Current_CD__project%
+browser-sync start --server --files "*/**, *.*"
+cd ..
+start _project\
+
+
+
+
+
+
